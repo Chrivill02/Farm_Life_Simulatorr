a6ec3fc2a0c59fb92f63451c6967b3dbe26c0d18
Christian Villegas <99997132+Chrivill02@users.noreply.github.com>
2023-09-19 17:37:29 -0600

    Cree el archivo base del proyecto
    En la rama develop hice el archivo base para que cada quien trabaje

---
+++ Proyecto Granja.py
@@ -0,0 +1,25 @@
+#Espacio para Angie(Calendario y Planificación)
+
+
+
+
+
+
+
+#Espacio para Rodrigo (Economía y mercado)
+
+
+
+
+
+
+
+#Espacio para Karen(Animales)
+
+
+
+
+
+
+
+#Espacio para Chris (Cultivos)
